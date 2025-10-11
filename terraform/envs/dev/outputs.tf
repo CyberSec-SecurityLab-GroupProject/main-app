5a8f9dea12b519bc6f49cdfba815dcdf8601422f
Tazahnae <t.matthews@steamforme.org>
2025-10-11 00:13:28 +0000

    feat(security): add Terraform modules + CI draft

---
+++ terraform/envs/dev/outputs.tf
@@ -0,0 +1,3 @@
+output "waf_acl_arn"      { value = try(module.waf_api.web_acl_arn, null) }
+output "trail_bucket"     { value = try(module.cloudtrail.bucket_name, null) }
+output "gd_sns_topic_arn" { value = try(module.gd_alerts.topic_arn, null) }
---
+++ terraform/modules/gd_alerts/main.tf
@@ -0,0 +1,33 @@
+variable "alert_email" {
+  type    = string
+  default = ""
+}
+
+resource "aws_sns_topic" "gd_alerts" {
+  name = "seclab-guardduty-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  count     = length(var.alert_email) > 0 ? 1 : 0
+  topic_arn = aws_sns_topic.gd_alerts.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
+
+resource "aws_cloudwatch_event_rule" "gd_findings" {
+  name = "seclab-guardduty-findings"
+  event_pattern = jsonencode({
+    "source"      : ["aws.guardduty"],
+    "detail-type" : ["GuardDuty Finding"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "sns_target" {
+  rule      = aws_cloudwatch_event_rule.gd_findings.name
+  target_id = "sns"
+  arn       = aws_sns_topic.gd_alerts.arn
+}
+
+output "topic_arn" {
+  value = aws_sns_topic.gd_alerts.arn
+}
